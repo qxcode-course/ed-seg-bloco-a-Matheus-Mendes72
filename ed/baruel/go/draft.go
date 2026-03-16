a28d6f8ecf20f629750c03806818587517a1f65d
Matheus de Sousa Mendes <matheussmendes04@gmail.com>
2026-03-16 15:22:12 -0300

    baruel finalizado

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -4,10 +4,50 @@ func main() {
     var qtd_total, baruel int
     fmt.Scan(&qtd_total, &baruel)
 
-    // figurinhas := make([]int, qtd_total)
-    // for i := 0; i < baruel; i++ {
+    figurinhas := make(map[int]int)
+    for i := 0; i < baruel; i++ {
+        var val_figur int
+        fmt.Scan(&val_figur)
+        figurinhas[val_figur]++
+    }
 
-    //     figurinhas[i] = baruel
-    // }
+    fig_rep := false
+    prim := true
+    for i := 0; i < baruel; i++ {
+        if figurinhas[i] > 1 {
+            for j := 0; j < figurinhas[i]-1; j++ {
+                    if !prim {
+                        fmt.Print(" ")
+                    }
+                fmt.Print(i)
+                fig_rep = true
+                prim = false
+            }
+        }
+    }
 
+    if !fig_rep {
+        fmt.Print("N")
+    }
+
+    fmt.Println()
+
+    prim = true
+    faltando := false
+    for i := 1; i <= qtd_total; i++ {
+        if figurinhas[i] == 0 {
+            if !prim {
+                fmt.Print(" ")
+            }
+            fmt.Print(i)
+            faltando = true
+            prim = false
+        }
+    }
+
+    if !faltando {
+        fmt.Print("N")
+    }
+
+    fmt.Println()
 }
